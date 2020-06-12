1e9698904f3b49690737feafa7d396d5bc46bcad
Fabi <fabiana@rotacloud.com>
2020-06-12 12:32:12 +0100

    First commit

---
+++ src/app/hero.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HeroService {
+  getHeroes(): Observable<Hero[]> {
+    return HEROES;
+  }
+
+  constructor(private messageService: MessageService) {} //service in service injected into the HeroesComponent
+  
+}
