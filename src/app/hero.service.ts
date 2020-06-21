929836892afe7a8753e6b0789bc555f1164c1b46
Fabi <fabiana.asara@gmail.com>
2020-06-21 18:20:02 +0100

    WIP : HttpClient request

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,21 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
+  private heroesUrl = 'api/heroes';  // URL to web api
 
-  constructor(private messageService: MessageService) {} //service-in-service scenario => MessageService into the HeroService which is injected into the HeroesComponent
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) {} 
 
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl) //get heroes from the server
+    .pipe(
+      catchError(this.HandleError<Hero[]>('getHeroes', []))
+    );
+
   }
 
   getHero(id: number): Observable<Hero> {
@@ -23,5 +30,9 @@ export class HeroService {
     id=${id}` ); //send the message after fetching the hero
     return of(HEROES.find(hero => hero.id === id));
   }
-  
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }  // this is wrapped in a private log since the Message service get called frequently 
+ 
 }
